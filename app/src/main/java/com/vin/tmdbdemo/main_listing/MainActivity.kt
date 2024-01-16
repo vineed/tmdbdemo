1e5135c1ee0bb91603e454f5b9805593a10c5d97
Vineed Gangadharan <vineedgangadharan@hotmail.com>
2024-01-17 01:50:48 +0530

    added domain module

--- app/src/main/java/com/vin/tmdbdemo/main_listing/MainActivity.kt
+++ app/src/main/java/com/vin/tmdbdemo/main_listing/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.vin.tmdbdemo
+package com.vin.tmdbdemo.main_listing
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -22,25 +22,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TmdbDemoTheme {
-        Greeting("Android")
     }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,4 +15,4 @@ dependencyResolutionManagement {
 
 rootProject.name = "TmdbDemo"
 include(":app")
- 
+include(":domain")
